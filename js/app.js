894b537005d8aa18152a6258d58f5cba6e3558f3
Tohir Raihan <tohir.raihan@gmail.com>
2021-09-13 03:09:30 +0600

    add rate and count in product

--- js/app.js
+++ js/app.js
@@ -24,6 +24,15 @@ const showProducts = (products) => {
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
         <h2>Price: $ ${product.price}</h2>
+        <div class="rating">
+          <div class="rate">
+            Rate: ${product.rating?.rate} <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+          </div>
+          <div class="count">
+            Count: ${product.rating?.count} <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+
+          </div>
+        </div>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button id="details-btn" class="btn btn-danger">Details</button>
       </div>
